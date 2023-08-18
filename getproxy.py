9370b5bbaca637c9a107b3de523f2f7ef1473fe2
Testne1 <125065172+Testne1@users.noreply.github.com>
2023-08-18 14:27:00 +0700

    getproxy.py

--- getproxy.py
+++ getproxy.py
@@ -3,7 +3,19 @@ import requests
 proxies = []
 
 # Get proxies from raw text files
-raw_proxy_sites = ['https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt', 'https://raw.githubusercontent.com/zloi-user/hideip.me/main/https.txt',' https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/https/https.txt',' https://raw.githubusercontent.com/UptimerBot/proxy-list/main/proxies/http.txt']
+raw_proxy_sites = [ "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+    "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-https.txt",
+    "https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-http.txt",
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+    "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+    "https://www.proxy-list.download/api/v1/get?type=http",
+    "https://www.proxy-list.download/api/v1/get?type=https",
+    "https://www.proxyscan.io/download?type=http",
+    "https://www.proxyscan.io/download?type=https",
+    "https://api.openproxylist.xyz/http.txt"]
 for site in raw_proxy_sites:
     response = requests.get(site)
     for line in response.text.split('\n'):
